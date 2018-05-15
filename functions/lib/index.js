27992a14c3a2d55f5fa387e85dbe25df91a5b72a
ChielBroere <chielbroere@gmail.com>
2018-05-15 18:41:50 +0200

    Update gitignore

--- functions/lib/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const functions = require("firebase-functions");
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
-//# sourceMappingURL=index.js.map
