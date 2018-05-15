17d09db51cd3389ef7f98be5fd644d0c3cc1d37c
ChielBroere <chielbroere@gmail.com>
2018-05-15 22:19:01 +0200

    Add pubsub functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,31 @@
+import { base64Decode } from '@firebase/util';
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as PubSub from '@google-cloud/pubsub';
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+const pubsub = new PubSub();
+
+admin.initializeApp(functions.config().firebase)
+
+var db = admin.firestore();
+
+export const sendPubMessage = functions.https.onRequest((request, response) => {
+    const topic = pubsub.topic(request.body.topic);
+    const publisher = topic.publisher();
+
+    response.contentType('application/json')
+
+    publisher.publish(Buffer.from(request.body.message))
+      .then(() => response.status(200).send())
+      .catch((err) => {
+        console.error(err);
+        response.status(500).send(err);
+    });
+});
+
+export const pubSubPuller = functions.pubsub.topic('iot-topic').onPublish((message) => {
+    db.collection('pubsub').doc().set({message : base64Decode(message.data)})
+    .catch(err => {
+      console.error(err)
+    });
 });
