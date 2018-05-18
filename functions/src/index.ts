72f9207e26f8864e7b2f3db1457a0b05213ef6d5
ChielBroere <Chielbroere@gmail.com>
2018-05-18 11:53:43 +0200

    Add firebase hosting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,8 +2,11 @@ import { base64Decode } from '@firebase/util';
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as PubSub from '@google-cloud/pubsub';
+import * as express from 'express';
 
 const pubsub = new PubSub();
+const API_PREFIX = 'api';
+const app = express();
 
 admin.initializeApp(functions.config().firebase)
 
@@ -29,3 +32,16 @@ export const pubSubPuller = functions.pubsub.topic('iot-topic').onPublish((messa
       console.error(err)
     });
 });
+
+app.use((req, res, next) => {
+    if (req.url.indexOf(`/${API_PREFIX}/`) === 0) {
+        req.url = req.url.substring(API_PREFIX.length + 1);
+    }
+    next();
+});
+
+app.post('/pubMessage', (req, res) => {
+    sendPubMessage(req, res);
+});
+
+exports[API_PREFIX] = functions.https.onRequest(app);
